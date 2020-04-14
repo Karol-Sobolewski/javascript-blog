5d927ce73222b6d48538083c5928997a30f86616
Karol <karolsobolewski92@gmail.com>
2020-04-14 19:28:46 +0200

    Install ESLint and EdutorConfir and StyleLint & Fix code in index.html and style.sass and script.js

--- js/script.js
+++ js/script.js
@@ -12,7 +12,7 @@
     const activeLinks = document.querySelectorAll('.titles a.active');
 
     for(let activeLink of activeLinks){
-        activeLink.classList.remove('active');
+      activeLink.classList.remove('active');
     }
 
     /* add class 'active' to the clicked link */
@@ -25,7 +25,7 @@
     const activeArticles = document.querySelectorAll('.post.active');
 
     for(let activeArticle of activeArticles){
-        activeArticle.classList.remove('active');
+      activeArticle.classList.remove('active');
     }
 
     /* get 'href' attribute from the clicked link */
@@ -40,34 +40,34 @@
 
     targetArticle.classList.add('active');
 
-  }
+  };
 
   const optArticleSelector = '.post',
-  optTitleSelector = '.post-title',
-  optTitleListSelector = '.titles';
+    optTitleSelector = '.post-title',
+    optTitleListSelector = '.titles';
   let html = "";
   function generateTitleLinks(){
 
     /* remove contents of titleList */
-    
-    const titleList = document.querySelector(optTitleListSelector); 
-  
+
+    const titleList = document.querySelector(optTitleListSelector);
+
     /* for each article */
 
     const articles = document.querySelectorAll(optArticleSelector);
-    
+
     for (let article of articles) {
 
       /* get the article id */
-      
+
       const getArticleId =  article.getAttribute("id");
 
       /* find the title element & get the title from the title elemen*/
-  
+
       const articleTitle = article.querySelector(optTitleSelector).innerHTML;
-  
+
       /* create HTML of the link */
-  
+
       const linkHTML = '<li><a href="#' + getArticleId + '"><span>' + articleTitle + '</span></a></li>';
 
       /* insert link into titleList */
@@ -76,9 +76,9 @@
 
       html = html + linkHTML;
 
-    }  
-    
-    titleList.innerHTML = html; 
+    }
+
+    titleList.innerHTML = html;
     const links = document.querySelectorAll('.titles a');
 
     for(let link of links){
@@ -86,7 +86,7 @@
     }
 
   }
-  
+
   generateTitleLinks();
-  
+
 }
